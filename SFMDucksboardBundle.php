d1041290b00f50cccb6b4da5004469d9effa13ed
Moises Gallego <moisesgallego@gmail.com>
2012-07-27 10:42:46 +0200

    added the bundle.php file

---
+++ SFMDucksboardBundle.php
@@ -0,0 +1,22 @@
+<?php
+
+/*
+ * This file is part of the DucksboardBundle package.
+ *
+ * (c) Moises Gallego <Twitter @moisesgallego>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace SFM\DucksboardBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+/**
+ * @author Moises Gallego <@moisesgallego>
+ */
+class SFMDucksboardBundle extends Bundle
+{
+ 
+}
