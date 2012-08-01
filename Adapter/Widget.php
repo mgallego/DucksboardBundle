926841ea1f363b249adeaed2a603cfd381cbc6a7
Moises Gallego <moisesgallego@gmail.com>
2012-08-01 16:44:51 +0200

    added a new method addToCounter

--- Adapter/Widget.php
+++ Adapter/Widget.php
@@ -24,6 +24,20 @@ class Widget extends WidgetHandler{
                 return json_decode($this->callApi($apiPath, 'GET'));
         }
 
+	public function addToCounter($widgetId)
+        {
+            $currentValue = 0;
+            $currentWidgetData = $this->getLastValues($widgetId, 1);
+
+            if ($currentWidgetData->data){
+                $currentValue = $currentWidgetData->data[0]->value;
+            }
+
+            $widgetData = array($widgetId => array('value' => $currentValue + 1 ));
+            $this->setData($widgetData);
+            $this->push();
+        }
+
         public function setData($data){
                 $this->data = $data;
         }
