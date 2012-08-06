c972c37d4ba2898e8756db8764bee7f0cf2e8503
Moises Gallego <moisesgallego@gmail.com>
2012-08-06 13:01:37 +0200

    added the SF2 serialize component in the push method

--- Adapter/WidgetHandler.php
+++ Adapter/WidgetHandler.php
@@ -2,59 +2,67 @@
 
 namespace SFM\DucksboardBundle\Adapter;
 
+use Symfony\Component\Serializer\Serializer;
+use Symfony\Component\Serializer\Encoder\JsonEncoder;
+use Symfony\Component\Serializer\Normalizer\GetSetMethodNormalizer;
 use SFM\DucksboardBundle\Adapter\Exception\DucksboardPushException;
 
 class WidgetHandler{
 
-        protected $apiKey;
-        private $pushApiPath = 'https://push.ducksboard.com/values/';
-        private $pullApiPath = 'https://pull.ducksboard.com/values/';
-        protected $data;
+    protected $apiKey;
+    private $pushApiPath = 'https://push.ducksboard.com/values/';
+    private $pullApiPath = 'https://pull.ducksboard.com/values/';
+    protected $data;
         
-        public function push(){
-                $data = $this->data;
-                foreach ($data as $widgetId => $widgetData){
-                        $apiPath = $this->pushApiPath . $widgetId;
-                        $retData = $this->callApi($apiPath, 'POST', json_encode($widgetData));
-                        try{
-                                if (json_decode($retData)->response !== 'ok'){
-                                        return false;
-                                }
-                        }
-                        catch(\ErrorException $e){
-                                throw new DucksboardPushException($apiPath, $retData);
-                        }
-                }
-                return true;
-        }
+    public function push(){
+	$encoders = array('json' => new JsonEncoder());
+	$normalizers = array(new GetSetMethodNormalizer());
+	$serializer = new Serializer($normalizers, $encoders);
 
-        public function callApi($apiPath, $method,  $inputData = null){ 
-                $ch = curl_init($apiPath);
-                curl_setopt($ch, CURLOPT_USERPWD, $this->apiKey.":ignored");
-                if ($inputData){
-                        curl_setopt ($ch, CURLOPT_POSTFIELDS, $inputData);
-                }
-                if ($method === 'POST'){
-                        curl_setopt ($ch, CURLOPT_POST, 1);
-                }elseif ($method === 'GET'){
-                        curl_setopt ($ch, CURLOPT_POST, 0);
-                }
-                curl_setopt ($ch,CURLOPT_RETURNTRANSFER,true);
-                $outData = curl_exec ($ch);
-                curl_close($ch);
+	$data = $this->data;
+	foreach ($data as $widgetId => $widgetData){
+	    $apiPath = $this->pushApiPath . $widgetId;
+	    $retData = $this->callApi($apiPath, 'POST', $serializer->serialize($widgetData, 'json'));
+	    $retResponse = $serializer->decode($retData, 'json');
+	    try{
+		if ($retResponse['response'] !== 'ok'){
+		    return false;
+		}
+	    }
+	    catch(\ErrorException $e){
+		throw new DucksboardPushException($apiPath, $retData);
+	    }
+	}
+	return true;
+    }
 
-                return $outData;
-        } 
+    public function callApi($apiPath, $method,  $inputData = null){ 
+	$ch = curl_init($apiPath);
+	curl_setopt($ch, CURLOPT_USERPWD, $this->apiKey.":ignored");
+	if ($inputData){
+	    curl_setopt ($ch, CURLOPT_POSTFIELDS, $inputData);
+	}
+	if ($method === 'POST'){
+	    curl_setopt ($ch, CURLOPT_POST, 1);
+	}elseif ($method === 'GET'){
+	    curl_setopt ($ch, CURLOPT_POST, 0);
+	}
+	curl_setopt ($ch,CURLOPT_RETURNTRANSFER,true);
+	$outData = curl_exec ($ch);
+	curl_close($ch);
 
-        public function setApiKey($apiKey){
-                $this->apiKey = $apiKey;
-        }
+	return $outData;
+    } 
 
-        public function getPushApiPath(){
-                return $this->pushApiPath;
-        }
+    public function setApiKey($apiKey){
+	$this->apiKey = $apiKey;
+    }
 
-        public function getPullApiPath(){
-                return $this->pullApiPath;
-        }
+    public function getPushApiPath(){
+	return $this->pushApiPath;
+    }
+
+    public function getPullApiPath(){
+	return $this->pullApiPath;
+    }
 }
